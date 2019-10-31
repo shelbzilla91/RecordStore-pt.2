06006ac0b49ed7d94ae7cfcef3fef8b24f2140cf
shelbzilla91 <shelbgatozillatron@gmail.com>
2019-10-30 23:21:48 -0500

    2 files modified

---
+++ components/UserForm.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import styled from 'styled-components';
+
+
+class UserForm extends Component {
+
+  render(){
+
+
+
+  return(
+<div>
+
+
+</div>
+
+  
+  )}}
+
+  export default UserForm;
